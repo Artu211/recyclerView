730ae3093b995b81eed4b209a86567ae9012a40b
Artu211 <160549068+Artu211@users.noreply.github.com>
2025-10-28 20:46:52 +0100

    Update UserAdapter.java

--- app/src/main/java/com/example/recyclerview/UserAdapter.java
+++ app/src/main/java/com/example/recyclerview/UserAdapter.java
@@ -3,11 +3,16 @@ package com.example.recyclerview;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
-import androidx.annotation.NonNull;
+
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
+import android.widget.Toast;
+
+import org.jspecify.annotations.NonNull;
+
 
 public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder> {
 
@@ -17,7 +22,6 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder
         this.userList = userList;
     }
 
-    // Ta metoda tworzy nowy ViewHolder, gdy RecyclerView go potrzebuje.
     @NonNull
     @Override
     public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -25,27 +29,36 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder
         return new UserViewHolder(view);
     }
 
-    // Ta metoda wypełnia ViewHolder danymi dla określonej pozycji na liście.
     @Override
     public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
         User currentUser = userList.get(position);
         holder.firstNameText.setText(currentUser.getFirstName());
-//        holder.lastNameText.setText(currentUser.getLastName());
+        holder.lastNameText.setText(currentUser.getLastName());
+        holder.avatarImage.setImageResource(currentUser.getAvatarResId());
+
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = "Kliknięto: " + currentUser.getFirstName() + " " + currentUser.getLastName();
+                Toast.makeText(v.getContext(), message, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
-    // Ta metoda zwraca liczbę elementów na liście.
     @Override
     public int getItemCount() {
         return userList.size();
     }
 
-    // Wewnętrzna klasa ViewHolder przechowuje referencje do widoków wiersza.
     public static class UserViewHolder extends RecyclerView.ViewHolder {
+        public ImageView avatarImage;
         public TextView firstNameText;
         public TextView lastNameText;
 
         public UserViewHolder(@NonNull View itemView) {
             super(itemView);
+            avatarImage = itemView.findViewById(R.id.avatarImage);
             firstNameText = itemView.findViewById(R.id.firstNameText);
             lastNameText = itemView.findViewById(R.id.lastNameText);
         }
